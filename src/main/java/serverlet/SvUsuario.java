c9b8ab500baf50491fae20357c2fdddf4c9ec0d9
JorgeSolano28 <jorge.solanobarboza28@gmail.com>
2026-04-02 23:07:42 -0600

    Cambios generales

--- src/main/java/serverlet/SvUsuario.java
+++ src/main/java/serverlet/SvUsuario.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package serverlet;
 
 import java.io.IOException;
@@ -12,10 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author jorge
- */
 @WebServlet(name = "SvUsuario", urlPatterns = {"/SvUsuario"})
 public class SvUsuario extends HttpServlet {
 
@@ -41,12 +33,20 @@ public class SvUsuario extends HttpServlet {
             throws ServletException, IOException {
         processRequest(request, response);
     }
+    
+    
+    
+    
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
+    
+    
+    
+    
 
     @Override
     public String getServletInfo() {
